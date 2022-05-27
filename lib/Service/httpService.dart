d1c903f7690cb4227a78b1ddd1e65dab01594539
Eduardo913 <cesarvilla903@gmail.com>
2022-05-27 10:27:28 -0500

    correction error and implement principal page

--- lib/Service/httpService.dart
+++ lib/Service/httpService.dart
@@ -4,27 +4,27 @@ import 'package:http/http.dart' as http;
 
 Future<String> buscar_pokemon(String id) async {
   final response =
-      await http.get(Uri.parse("http://localhost:5000/api/pokemon/$id"));
+      await http.get(Uri.parse("http://184.73.131.171/api/pokemon/$id"));
   return json.decode(response.body)["forms"][0]["name"];
   // return http.get(Uri.parse("http://localhost:5000/api/pokemon/$id"));
 }
 
 Future<String> suma_http(String number1, String number2) async {
   final response = await http.get(Uri.parse(
-      "http://localhost:5000/api/calculadora/suma/$number1/$number2/"));
+      "http://184.73.131.171/api/calculadora/suma/$number1/$number2/"));
   return response.body;
 }
 
 Future<String> resta_http(String number1, String number2) async {
   final response = await http.get(Uri.parse(
-      "http://localhost:5000/api/calculadora/resta/$number1/$number2/"));
+      "http://184.73.131.171/api/calculadora/resta/$number1/$number2/"));
 
   return response.body;
 }
 
 Future<String> multiplicacion_http(String number1, String number2) async {
   final response = await http.get(Uri.parse(
-      "http://localhost:5000/api/calculadora/multiplicacion/$number1/$number2/"));
+      "http://184.73.131.171/api/calculadora/multiplicacion/$number1/$number2/"));
 
   return response.body;
 }
